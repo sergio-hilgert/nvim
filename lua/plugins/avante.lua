95a797aba5ad7c66e462b4dc764c19d9d32df0a3
Sergio Hilgert <hilgertfacul@gmail.com>
2026-01-04 01:04:05 -0300

    increase avante max tokens

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -25,7 +25,7 @@ return {
         context_window = 200000,
         extra_request_body = {
           temperature = 0.45,
-          max_tokens = 8192,
+          max_tokens = 30000,
         },
       },
     },
