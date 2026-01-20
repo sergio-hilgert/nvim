32c202f6ad065e10f72e13605c2b13d03b8224f8
Sergio Hilgert <hilgertfacul@gmail.com>
2026-01-20 20:57:51 -0300

    Update CMP to be less agressive

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,19 +1,53 @@
 return {
   {
-    "nvim-cmp",
-    -- after the global opts, add a Ruby-only override
-    -- config = function(_, _)
-    --   local cmp = require("cmp")
-    --   -- grab the default list of sources
-    --   local default_sources = cmp.get_config().sources
-    --
-    --   -- override only for Ruby buffers
-    --   cmp.setup.filetype("ruby", {
-    --     -- filter out luasnip
-    --     sources = vim.tbl_filter(function(src)
-    --       return src.name ~= "luasnip"
-    --     end, default_sources),
-    --   })
-    -- end,
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+
+      -- Make completion less aggressive
+      opts.completion = {
+        -- Only show completion after typing 3 characters
+        keyword_length = 3,
+      }
+
+      -- Limit the completion window size
+      opts.window = {
+        completion = cmp.config.window.bordered({
+          max_height = 10,  -- Limit to 10 items visible
+          max_width = 50,   -- Limit width
+        }),
+        documentation = cmp.config.window.bordered({
+          max_height = 15,
+          max_width = 50,
+        }),
+      }
+
+      -- Add delay before showing completions (in milliseconds)
+      opts.performance = {
+        debounce = 150,        -- Wait 150ms after typing before showing completions
+        throttle = 50,         -- Minimum time between completion updates
+        fetching_timeout = 200,
+      }
+
+      -- Limit the number of items shown
+      opts.matching = {
+        disallow_fuzzy_matching = false,
+        disallow_fullfuzzy_matching = false,
+        disallow_partial_fuzzy_matching = false,
+        disallow_partial_matching = false,
+        disallow_prefix_unmatching = false,
+      }
+
+      -- Configure sources with lower priority and keyword length
+      opts.sources = cmp.config.sources({
+        { name = "nvim_lsp", keyword_length = 3, max_item_count = 10 },
+        { name = "luasnip", keyword_length = 3, max_item_count = 5 },
+        { name = "buffer", keyword_length = 4, max_item_count = 5 },
+        { name = "nvim_lua", keyword_length = 3, max_item_count = 5 },
+        { name = "path", keyword_length = 2, max_item_count = 5 },
+      })
+
+      return opts
+    end,
   },
 }
